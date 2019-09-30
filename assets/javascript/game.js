9077367f540740efff8eee999b89672481c1fe34
Romy Owens <rdoed160@gmail.com>
2019-09-29 21:37:49 -0400

    pseudo code js

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// computer guessed score between 19 - 120 points
+// random value assigned to each crystal between 1 - 12
+// collect player score and displa it
+// if user score matches computer guess player wins
+// if user score is greater than computer guess player loss
+// after win/loss reset computer guess and crystal values to new variables

d299c8993b606b1f5f6648dd1b0b4970129f9773
Romy Owens <rdoed160@gmail.com>
2019-10-01 10:00:56 -0400

    add value numbers to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,29 +3,38 @@ var losses = 0;
 var userScore = [];
 
 
+
 $(document).ready(function() {
-
- var compScore = Math.floor(Math.random() * 101) + 19;
-
-$("comp-guess").text(compScore);
-
-
-
 // computer guessed score between 19 - 120 points
 // var randomScore = Math.floor(Math.random() * 101) + 19;
-$("#click-me").on("click", function() {
-    alert("I've been clicked!");
+ var compScore = Math.floor(Math.random() * 101) + 19;
 
-// $(".guessed-score").text(randomScore);
-console.log("guessed-score");
+$("#comp-guess").text(compScore);
+
 
 // random value assigned to each crystal between 1 - 12
+var redCrystal = Math.floor(Math.random() * 11) + 1;
+
+
+var blueCrystal = Math.floor(Math.random() * 11) + 1;
+
+
+var yellowCrystal = Math.floor(Math.random() * 11) + 1;
+
+
+var greenCrystal = Math.floor(Math.random() * 11) + 1;
+
+
+// $(".guessed-score").text(randomScore);
+
+
+
 
 // collect player score and displa it
 // if user score matches computer guess player wins
 // if user score is greater than computer guess player loss
 // after win/loss reset computer guess and crystal values to new variables
-}
+});
 
 // $(document).ready(function(){
     // $("button").click(function(){

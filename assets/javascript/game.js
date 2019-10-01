0977dcaed82f3bec45a9300ab6e9dd56bab4a805
Romy Owens <rdoed160@gmail.com>
2019-10-01 09:36:04 -0400

    add js coding for computer score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,33 @@
+var wins = 0;
+var losses = 0;
+var userScore = [];
+
+
+$(document).ready(function() {
+
+ var compScore = Math.floor(Math.random() * 101) + 19;
+
+$("comp-guess").text(compScore);
+
+
+
 // computer guessed score between 19 - 120 points
+// var randomScore = Math.floor(Math.random() * 101) + 19;
+$("#click-me").on("click", function() {
+    alert("I've been clicked!");
+
+// $(".guessed-score").text(randomScore);
+console.log("guessed-score");
+
 // random value assigned to each crystal between 1 - 12
+
 // collect player score and displa it
 // if user score matches computer guess player wins
 // if user score is greater than computer guess player loss
 // after win/loss reset computer guess and crystal values to new variables
+}
+
+// $(document).ready(function(){
+    // $("button").click(function(){
+    //   $("p").text("Hello world!");
+    // });
